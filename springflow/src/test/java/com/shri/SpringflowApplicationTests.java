c6c573f4ba6798a5602616728d3bef17e8dcd197
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2024-12-11 15:36:50 +0530

    project setup

---
+++ springflow/src/test/java/com/shri/SpringflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
