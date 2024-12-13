b58dced9f9107b1341879c6acc7ec1c045f442d2
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2024-12-13 12:06:50 +0530

    JWT complete

--- springflow/src/main/java/config/JwtTokenValidator.java
+++ springflow/src/main/java/config/JwtTokenValidator.java
@@ -7,8 +7,12 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.crypto.SecretKey;
@@ -36,13 +40,19 @@ public class JwtTokenValidator  extends OncePerRequestFilter {
 
                     List<GrantedAuthority> auth= AuthorityUtils.commaSeparatedStringToAuthorityList(authorities);
 
-                    a
+                   Authentication authentication =new UsernamePasswordAuthenticationToken(email,null,auth);
+                   SecurityContextHolder.getContext().setAuthentication(authentication);
+
 
                }
                catch (Exception e)
                {
-
+                          throw new BadCredentialsException("invalid token");
                }
+
            }
+
+
+           filterChain.doFilter(request,response);
     }
 }
