54324b14e018b897c8740b9802f786c36538adfc
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2024-12-14 22:15:20 +0530

    Issue, chat , message models complete

--- springflow/src/main/java/com/shri/model/Chat.java
+++ springflow/src/main/java/com/shri/model/Chat.java
@@ -1,10 +1,15 @@
 package com.shri.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.security.PrivateKey;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -16,6 +21,16 @@ public class Chat {
     private Long id;
 
 
+    private String name;
+
     @OneToOne
     private  Project project;
+
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "chat",cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<Message> message;
+
+    @ManyToMany
+    private  List<User> users=new ArrayList<>();
 }
--- springflow/src/main/java/com/shri/model/Message.java
+++ springflow/src/main/java/com/shri/model/Message.java
@@ -1,29 +1,30 @@
 package com.shri.model;
 
-
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Comment {
+public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private  Long id;
-    private String content;
+    private Long id;
 
-     private LocalDateTime createdDate;
+    private  String content;
 
-     @ManyToOne
-     private  User user;
+    private LocalDateTime createdAt;
 
     @ManyToOne
-    private  Issue issue;
+    private  Chat chat;
+
+
+    @ManyToOne
+    private  User sender;
+
 }
